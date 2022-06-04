99ad5513d7b998cf442ef19aee8ecf0ad007bd37
David Swearingen <swearingendw@mac.com>
2022-06-04 12:57:21 -0400

    progress on writing local time to spreadsheet

--- my_ttgo_esp32_post/my_ttgo_esp32_post.ino
+++ my_ttgo_esp32_post/my_ttgo_esp32_post.ino
@@ -29,14 +29,20 @@ WiFiMulti wifiMulti;  // wifiMulti is able to pick best of multiple AP's but thi
 int sensorValue = 0;
 int sensorPin = 35;
 
-void printLocalTime()
+
+String returnLocalTime()
 {
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
-    return;
+    return "xxx";
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  // Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  char timeStringBuff[50];
+
+  // "A": Saturday
+  strftime(timeStringBuff, sizeof(timeStringBuff), "%B %d %Y %H:%M:%S", &timeinfo);
+  return String(timeStringBuff);
 }
 
 void setup() {
@@ -54,58 +60,57 @@ void loop() {
  
 void postDataToServer(int mSecs) {
 
-
   Serial.println("Posting JSON data to server...");
   // Block until we are able to connect to the WiFi access point
   if (wifiMulti.run() == WL_CONNECTED) {
 
   //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+  configTime(gmtOffset_sec * -5, daylightOffset_sec, ntpServer);
+  Serial.print(returnLocalTime());
      
-    HTTPClient http;   
+  HTTPClient http;   
+    
+  http.begin(SECRET_ZAPIER_ENDPOINT);  
+  http.addHeader("Content-Type", "application/json");         
+    
+  StaticJsonDocument<200> doc;
+  // Add values in the document
+  // This is the payload.  Each item below maps to a column in the spreadsheet.
+  // So if you have a spreadsheet with a column called "timestamp", then you 
+  // must set doc["timestamp"] = "03/24/1988" for example.
+  doc["name"] = "david-6";
+  // doc["location"] = String(random(105));  
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Sensor value:");
+  Serial.println(sensorValue);
+  doc["location"] = String(sensorValue);
+  
+  // Add an array
+  // JsonArray data = doc.createNestedArray("data");
+  // data.add(48.756080);
+  // data.add(2.302038);
      
-    http.begin(SECRET_ZAPIER_ENDPOINT);  
-    http.addHeader("Content-Type", "application/json");         
-     
-    StaticJsonDocument<200> doc;
-    // Add values in the document
-    // This is the payload.  Each item below maps to a column in the spreadsheet.
-    // So if you have a spreadsheet with a column called "timestamp", then you 
-    // must set doc["timestamp"] = "03/24/1988" for example.
-    doc["name"] = "david-6";
-    // doc["location"] = String(random(105));  
-    sensorValue = analogRead(sensorPin);
-    Serial.println("Sensor value:");
-    Serial.println(sensorValue);
-    doc["location"] = String(sensorValue);
-   
-    // Add an array
-    // JsonArray data = doc.createNestedArray("data");
-    // data.add(48.756080);
-    // data.add(2.302038);
-     
-    String requestBody;
-    serializeJson(doc, requestBody);
-     
-    int httpResponseCode = http.POST(requestBody);
- 
-    if(httpResponseCode>0){
-       
-      String response = http.getString();                       
-      Serial.println("HTTP response code:");
-      Serial.println(httpResponseCode);  
-      Serial.println("HTTP response"); 
-      Serial.println(response);
-     
-    }
-    else {
-     
-      //Serial.printf("Error occurred while sending HTTP POST: %s\n", httpClient.errorToString(statusCode).c_str());
-      Serial.printf("Error occurred while sending HTTP POST\n");
-       
-    }
-    delay(mSecs);
+  String requestBody;
+  serializeJson(doc, requestBody);
+    
+  int httpResponseCode = http.POST(requestBody);
+
+  if(httpResponseCode>0){
+      
+    String response = http.getString();                       
+    Serial.println("HTTP response code:");
+    Serial.println(httpResponseCode);  
+    Serial.println("HTTP response"); 
+    Serial.println(response);
+    
+  }
+  else {
+    
+    //Serial.printf("Error occurred while sending HTTP POST: %s\n", httpClient.errorToString(statusCode).c_str());
+    Serial.printf("Error occurred while sending HTTP POST\n");
+      
+  }
+  delay(mSecs);
      
   }
 }
